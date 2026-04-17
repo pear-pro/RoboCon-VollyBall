c53d7b16b74178c8640089456b793ef5fa8e0a05
pear-pro <2925741372@qq.com>
2026-04-17 21:47:01 +0800

    day4,拍完了发球的视频可以再优化一下

--- USER/Inc/includes.h
+++ USER/Inc/includes.h
@@ -21,9 +21,9 @@
 #include "JY901P_Calibrate.h"
 
 #define damiao_1_back 	2.20f
-#define damiao_1_front 	-1.1f
+#define damiao_1_front 	-1.2f
 #define damiao_1_safe	0.5f
-#define damiao_0_up 	-0.35f
+#define damiao_0_up 	-0.38f
 
 extern motor_info_t C620[MotorCount];
 extern motor_info_t C6xx[MotorCount];
